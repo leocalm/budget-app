73aff3bf1d4c4d90857d0ec6501aeea9da449ec0
Leonardo Almeida <leocalm@gmail.com>
2026-02-03 13:20:14 +0100

    chore(ci): remove generated storybook-static from repo and add to .gitignore

--- storybook-static/assets/EditAccountForm.story-Cgx5qTuy.js
+++
@@ -1,9 +0,0 @@
-import{j as t}from"./iframe-BlPw6RHN.js";import{Q as e}from"./useAccounts-But_qxzc.js";import{m}from"./budgetData-Dm9Z5fBL.js";import{E as s}from"./EditAccountForm-D0nm6J6c.js";import{Q as a}from"./queryClient-Dw0i7u28.js";import"./preload-helper-PPVm8Dsz.js";import"./use-form-ae0NiLND.js";import"./polymorphic-factory-0CtcYZKl.js";import"./noop-BjFrJKj1.js";import"./TextInput-BD6MJsqd.js";import"./UnstyledButton-DT2tpwi4.js";import"./index-R61n_Ntc.js";import"./ScrollArea-DfS6V9t2.js";import"./create-safe-context-B7JJMNKb.js";import"./Loader-CTmnaFpN.js";import"./Stack-quhSlGT0.js";import"./Alert-DVjRT69K.js";import"./Grid-3nMlZOK-.js";import"./get-base-value-FMRBrurP.js";import"./ActionIcon-_QTefNip.js";import"./ColorSwatch-Cu4-wEOa.js";import"./Group-VLj-LUgQ.js";import"./Button-Bd-cDwKe.js";const i=new a,R={title:"Components/Accounts/EditAccountForm",component:s,argTypes:{onUpdated:{action:"updated"}},decorators:[c=>t.jsx(e,{client:i,children:t.jsx(c,{})})]},o={args:{account:m[0]}},r={args:{account:m[1]}};o.parameters={...o.parameters,docs:{...o.parameters?.docs,source:{originalSource:`{
-  args: {
-    account: mockAccounts[0]
-  }
-}`,...o.parameters?.docs?.source}}};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
-  args: {
-    account: mockAccounts[1]
-  }
-}`,...r.parameters?.docs?.source}}};export{o as Default,r as WithSavings,R as default};
--- storybook-static/assets/TransactionStats.story-CgaKkaDs.js
+++
@@ -1,7 +0,0 @@
-import{T as t}from"./TransactionStats-UD_ok_ae.js";import"./iframe-BlPw6RHN.js";import"./preload-helper-PPVm8Dsz.js";import"./useTranslation-CrzPYgp4.js";import"./index-tni0gN6T.js";import"./SimpleGrid-CH29pY9P.js";import"./polymorphic-factory-0CtcYZKl.js";import"./get-base-value-FMRBrurP.js";import"./Text-CKudE1NG.js";const l={title:"Components/Transactions/TransactionStats",component:t},e={args:{income:1e5,balance:1e4,expenses:9e4}};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`{
-  args: {
-    income: 100000,
-    balance: 10000,
-    expenses: 90000
-  }
-}`,...e.parameters?.docs?.source}}};export{e as Default,l as default};
