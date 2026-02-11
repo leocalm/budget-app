469aa61dc493d37c7e98269a19e765fdd74bb7d2
Leonardo Almeida <leocalm@gmail.com>
2026-02-11 16:12:20 +0100

    Fixing icon and logo

--- src/components/Layout/Logo.tsx
+++ src/components/Layout/Logo.tsx
@@ -1,5 +1,6 @@
 import { useTranslation } from 'react-i18next';
 import { Group, Image, Text, ThemeIcon } from '@mantine/core';
+import logo from '@/assets/icons/png/gradient/piggy-pulse.png';
 
 export function Logo() {
   const { t } = useTranslation();
@@ -7,7 +8,7 @@ export function Logo() {
   return (
     <Group gap="xs">
       <ThemeIcon size={40} radius="md" bg="transparent" variant="light">
-        <Image src="/assets/icons/png/gradient/piggy-pulse.png" />
+        <Image src={logo} />
       </ThemeIcon>
       <Text size="xl" fw={700} className="brand-text">
         {t('layout.logo.title')}
